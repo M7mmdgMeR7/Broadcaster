dcd48085fb74a7d1e9cf6442ffa9719611fc96db
M7mmdgMeR7 <Mohalaamer@icloud.com>
2016-04-16 10:58:35 +0300

    Update Commands.php

--- src/Broadcaster/Commands/Commands.php
+++ src/Broadcaster/Commands/Commands.php
@@ -39,11 +39,11 @@ class Commands extends PluginBase implements CommandExecutor{
     				if(isset($args[0])){
     			   		$args[0] = strtolower($args[0]);
     			   		if($args[0]=="reload"){
-    			   			if($sender->hasPermission("broadcaster.reload")) {
+    			   			if($sender->hasPermission("Tag1b.reload")) {
     			   				$this->plugin->reloadConfig();
     			   				$this->cfg = $this->plugin->getConfig()->getAll();
-    			   				$time = intval($this->cfg["time"]) * 20;
-    			   				$ptime = intval($this->cfg["popup-time"]) * 20;
+    			   				$time = intval($this->cfg["Tag block"]) * 20;
+    			   				$ptime = intval($this->cfg["Name block "]) * 20;
     			   				$this->plugin->task->remove();
     			   				$this->plugin->ptask->remove();
     			   				$this->plugin->task = $this->plugin->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this->plugin), $time);
@@ -67,7 +67,7 @@ class Commands extends PluginBase implements CommandExecutor{
     			   				return true;
     			   			}
     			   		}else{
-    			   			if($sender->hasPermission("broadcaster")){
+    			   			if($sender->hasPermission("Tag1b")){
     			   				$sender->sendMessage($this->plugin->translateColors("&", Main::PREFIX . "&cSubcommand &9" . $args[0] . "&c not found. Use &9/bc &cto show available commands"));
     			   				break;
     			   			}
@@ -80,11 +80,11 @@ class Commands extends PluginBase implements CommandExecutor{
     			   	}
     			   	else{
     			   		if($sender->hasPermission("broadcaster")){
-    			   			$sender->sendMessage($this->plugin->translateColors("&", "&2- &9Available Commands &2-"));
-    			   			$sender->sendMessage($this->plugin->translateColors("&", "&9/bc info &2- &9Show info about this plugin"));
-    			   			$sender->sendMessage($this->plugin->translateColors("&", "&9/bc reload &2- &9Reload the config"));
-    			   			$sender->sendMessage($this->plugin->translateColors("&", "&9/sendmessage &2- &9Send message to the specified player (* for all players)"));
-    			   			$sender->sendMessage($this->plugin->translateColors("&", "&9/sendpopup &2- &9Send popup to the specified player (* for all players)"));
+    			   			$sender->Color block($this->plugin->translateColors("&", "&2- &9Available Commands &2-"));
+    			   			$sender->Color Block($this->plugin->translateColors("&", "&9/bc info &2- &9Show info about this plugin"));
+    			   			$sender->Color block ($this->plugin->translateColors("&", "&9/bc reload &2- &9Reload the config"));
+    			   			$sender->Color block ($this->plugin->translateColors("&", "&9/sendmessage &2- &9Send message to the specified player (* for all players)"));
+    			   			$sender->color block ($this->plugin->translateColors("&", "&9/sendpopup &2- &9Send popup to the specified player (* for all players)"));
     			   			break;
     			   		}else{
     			   			$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
